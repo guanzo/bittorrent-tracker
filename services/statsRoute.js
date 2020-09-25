cbe4a3f35a78d5816c28844c02cebdca27390370
Jonathan El-Bizri <github@elbizri.com>
2020-09-25 13:28:36 -0700

    UPDATE: all route services extracted, tests pass

--- services/statsRoute.js
+++ services/statsRoute.js
@@ -1,3 +1,5 @@
+const peerid = require('bittorrent-peerid')
+
 const attachHttpServer = require('./attachHttp')
 
 const get8Chars = 
@@ -5,7 +7,7 @@ const get8Chars =
                         .toString()
                         .substring(0, 8)
               
-function addPeerClient( _client, peer) {
+function addPeerClient( _clients, peer) {
     const clientId = peer.client.client;
     const client = _clients[clientId] || {};
     // If the client is not known show 8 chars from peerId as version
@@ -48,52 +50,75 @@ function printClients(clients) {
   return html + "</ul>";
 }
 
-function addPeerId(allPeers, peerId) {
-  // Don't mark the peer as most recently used for stats
-  const peer = peers.peek(peerId);
+const countPeers =
+    allPeers =>
+        filterFunction => {
+            let count = 0
+            let key
 
-  if (peer == null) return; // peers.peek() can evict the peer
+            for (key in allPeers) {
+                if (hasOwnProperty.call(allPeers, key) && filterFunction(allPeers[key])) {
+                    count++
+                } 0
+            }
 
-  allPeers[peerId] = {
-    ipv6: peer.ip.includes(":"),
-    ipv4: !peer.ip.includes(":"),
-    seeder: peer.completee,
-    leecher: !peer.complete,
-    peerId: peer.peerId,
-    client: peerid(peer.peerId),
-  };
+            return count
+        }
 
-  return allPeers
+const addPeer =
+    peers =>
+        (allPeers, peerId) => {
+            // Don't mark the peer as most recently used for stats
+            const peer = peers.peek(peerId)
+
+            if (peer == null) return // peers.peek() can evict the peer
+
+            allPeers[peerId] = {
+                ipv4: false,
+                ipv6: false,
+                seeder: false,
+                leecher: false
+            }
+
+            if (peer.ip.includes(':')) allPeers[peerId].ipv6 = true
+            if (!peer.ip.includes(':')) allPeers[peerId].ipv4 = true
+
+            if (peer.complete) allPeers[peerId].seeder = true
+            if (!peer.complete) allPeers[peerId].leecher = true
+
+            allPeers[peerId].peerId = peer.peerId
+            allPeers[peerId].client = peerid(peer.peerId)
+
+            return allPeers
+        }
+
+const countActiveTorrents = torrents => {
+    const hasKeys = (torrent) => (torrent.peers.keys.length > 0) 
+
+    const activeTorrents = torrents.filter(hasKeys, 0)
+    return activeTorrents.length
 }
 
-function addTorrentStats(allPeers, torrent) {
-    const keys = torrent.peers.keys;
+const addTorrentKeys = (allPeers, torrent) => {
+    const peers = torrent.peers
+    const keys = peers.keys
 
-    const result = keys.reduce(addPeerId, allPeers)
+    const result = keys.reduce(addPeer(peers), allPeers)
 
     return result
 }
 
+const getTheStats = server => {
+    const torrents = Object.values(server.torrents)
+    const allPeers = torrents.reduce(addTorrentKeys, {})
 
-const countPeersF =
-        peers => 
-            filterFunction => 
-                Object.values(peers).filter(filterFunction).length
-
-const isActive = torrent => (torrent.peers.keys.length > 0)
-const countActive = torrents => torrents.filter(isActive).length
-const isSeederOnly = peer => (peer.seeder && peer.leecher === false);
-const isLeecherOnly = peer => (peer.leecher && peer.seeder === false);
-const isSeederAndLeecher = peer => (peer.seeder && peer.leecher);
-const isIPv4 = peer => peer.ipv4;
-const isIPv6 = peer => peer.ipv6;
-
-function getStats(server) {
-    const torrents = Object.values(server.torrents);
-    const allPeers = torrents.reduce(addTorrentStats, {})
-
-    const countAllPeers = countPeersF(allPeers);
-    const activeTorrents = countActive(torrents);
+    const activeTorrents = countActiveTorrents(Object.values(server.torrents))
+    const isSeederOnly = peer => (peer.seeder && peer.leecher === false)
+    const isLeecherOnly = peer => (peer.leecher && peer.seeder === false)
+    const isSeederAndLeecher = peer => (peer.seeder && peer.leecher)
+    const isIPv4 = peer => peer.ipv4
+    const isIPv6 = peer => peer.ipv6
+    const countAllPeers = countPeers(allPeers)
 
     const stats = {
         torrents: torrents.length,
@@ -104,34 +129,28 @@ function getStats(server) {
         peersSeederAndLeecher: countAllPeers(isSeederAndLeecher),
         peersIPv4: countAllPeers(isIPv4),
         peersIPv6: countAllPeers(isIPv6),
-        clients: groupByClient(allPeers),
-    };
+        clients: groupByClient(allPeers)
+    }
 
-    return stats;
+    return stats
 }
 
-function setupStatsRoute(server) {
-  if (!server.http) attachHttpServer(server);
+function setupStatsRoute(server, onListening) {
+  if (!server.http) attachHttpServer(server, onListening);
+   // Http handler for '/stats' route
+    server.http.on('request', (req, res) => {
+    if (res.headersSent) return
 
-  // Http handler for '/stats' route
-  server.http.on("request", (req, res) => {
-    if (res.headersSent) return;
+    if (req.method === 'GET' && (req.url === '/stats' || req.url === '/stats.json')) {
+        const stats = getTheStats(server)
 
-    const stats = getStats(server);     
+        if (req.url === '/stats.json' || req.headers.accept === 'application/json') {
 
-    if (
-        req.url === "/stats.json" ||
-        req.headers.accept === "application/json"
-    ) {
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify(stats));
-    } 
-    else if (req.url === "/stats") {
-        const stats = getStats(server);     
-
-        res.setHeader("Content-Type", "text/html");
-        res.end(
-            `
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify(stats))
+        } else if (req.url === '/stats') {
+        res.setHeader('Content-Type', 'text/html')
+        res.end(`
             <h1>${stats.torrents} torrents (${stats.activeTorrents} active)</h1>
             <h2>Connected Peers: ${stats.peersAll}</h2>
             <h3>Peers Seeding Only: ${stats.peersSeederOnly}</h3>
@@ -141,10 +160,10 @@ function setupStatsRoute(server) {
             <h3>IPv6 Peers: ${stats.peersIPv6}</h3>
             <h3>Clients:</h3>
             ${printClients(stats.clients)}
-        `.replace(/^\s+/gm, "")
-        ); // trim left
+        `.replace(/^\s+/gm, '')) // trim left
+        }
     }
-  });
+    })
 }
 
 module.exports = setupStatsRoute
