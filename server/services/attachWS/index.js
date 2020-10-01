ecd8594e29db462f68f9c44d14bb82f0c2dcda8c
Jonathan El-Bizri <github@elbizri.com>
2020-10-01 16:33:13 -0700

    WIP: standardjs linting, tracking down missing swarm error

--- server/services/attachWS/index.js
+++ server/services/attachWS/index.js
@@ -1,14 +1,14 @@
-const WebSocketServer = require("ws").Server
+const WebSocketServer = require('ws').Server
 const debug = require('debug')('bittorrent-tracker:server')
 
 const common = require('../../../lib/common')
 
-const parseWebSocketRequest = require("./parseWebsocketRequest")
-const attachHttpServer = require("../attachHttp")
+const parseWebSocketRequest = require('./parseWebsocketRequest')
+const attachHttpServer = require('../attachHttp')
 
 const SDP_TRICKLE_REGEX = /a=ice-options:trickle\s\n/
 
-function setupHttpService(server, onListening) {
+function setupHttpService (server, onListening) {
   if (server.http) return server.http
   else {
     attachHttpServer(server, onListening)
@@ -18,19 +18,19 @@ function setupHttpService(server, onListening) {
     // Return 404 for all other request types
     const notFound = (req, res) => {
       res.statusCode = 404
-      res.end("404 Not Found")
+      res.end('404 Not Found')
     }
 
-    server.http.on("request", notFound)
+    server.http.on('request', notFound)
   }
 }
 
-function setupWebSocketServer(server) {
+function setupWebSocketServer (server) {
   const { onError } = server
   const options = {
     server: server.http,
     perMessageDeflate: false,
-    clientTracking: false,
+    clientTracking: false
   }
 
   const ws = new WebSocketServer(options)
@@ -40,24 +40,16 @@ function setupWebSocketServer(server) {
 
     socket.peerId = null // as hex
     socket.infoHashes = [] // swarms that server socket is participating in
-    socket.onSend = (err) => {
-      onWebSocketSend(socket, err)
-    }
+    socket.onSend = (err) => onWebSocketSend(socket, err)
 
-    socket.onMessageBound = (params) => {
-      onWebSocketRequest(socket, opts, params)
-    }
-    socket.on("message", socket.onMessageBound)
+    socket.onMessageBound = (params) => onWebSocketRequest(socket, opts, params)
+    socket.on('message', socket.onMessageBound)
 
-    socket.onErrorBound = (err) => {
-      onWebSocketError(socket, err)
-    }
-    socket.on("error", socket.onErrorBound)
+    socket.onErrorBound = (err) => onWebSocketError(socket, err)
+    socket.on('error', socket.onErrorBound)
 
-    socket.onCloseBound = () => {
-      onWebSocketClose(socket)
-    }
-    socket.on("close", socket.onCloseBound)
+    socket.onCloseBound = () => onWebSocketClose(socket)
+    socket.on('close', socket.onCloseBound)
   }
 
   const onWebSocketRequest = (socket, opts, params) => {
@@ -66,14 +58,14 @@ function setupWebSocketServer(server) {
     } catch (err) {
       socket.send(
         JSON.stringify({
-          "failure reason": err.message,
+          'failure reason': err.message
         }),
         socket.onSend
       )
 
       // even though it's an error for the client, it's just a warning for the server.
       // don't crash the server because a client sent bad data :)
-      server.emit("warning", err)
+      server.emit('warning', err)
       return
     }
 
@@ -85,22 +77,22 @@ function setupWebSocketServer(server) {
         socket.send(
           JSON.stringify({
             action:
-              params.action === common.ACTIONS.ANNOUNCE ? "announce" : "scrape",
-            "failure reason": err.message,
-            info_hash: common.hexToBinary(params.info_hash),
+              params.action === common.ACTIONS.ANNOUNCE ? 'announce' : 'scrape',
+            'failure reason': err.message,
+            info_hash: common.hexToBinary(params.info_hash)
           }),
           socket.onSend
         )
 
-        server.emit("warning", err)
+        server.emit('warning', err)
         return
       }
 
       response.action =
-        params.action === common.ACTIONS.ANNOUNCE ? "announce" : "scrape"
+        params.action === common.ACTIONS.ANNOUNCE ? 'announce' : 'scrape'
 
       let peers
-      if (response.action === "announce") {
+      if (response.action === 'announce') {
         peers = response.peers
         delete response.peers
 
@@ -118,50 +110,35 @@ function setupWebSocketServer(server) {
       if (!params.answer) {
         socket.send(JSON.stringify(response), socket.onSend)
         debug(
-          "sent response %s to %s",
+          'sent response %s to %s',
           JSON.stringify(response),
           params.peer_id
         )
       }
 
       if (Array.isArray(params.offers)) {
-        debug("got %s offers from %s", params.offers.length, params.peer_id)
-        debug("got %s peers from swarm %s", peers.length, params.info_hash)
+        debug('got %s offers from %s', params.offers.length, params.peer_id)
+        debug('got %s peers from swarm %s', peers.length, params.info_hash)
 
-        const gotSwarm = swarm => {
-          if (server.destroyed) return
-          if (!swarm) {
-            return server.emit(
-              "warning",
-              new Error("no swarm with that `info_hash`")
-            )
+        peers.forEach((peer, i) => {
+          const { sdp } = (params.offers && params.offers[i] && params.offers[i].offer) || {}
+          const isTrickleSdp = SDP_TRICKLE_REGEX.test(sdp)
+          if (isTrickleSdp) {
+            swarm.offers.set(params.offers[i].offer_id, peer.peerId)
           }
 
-          peers.forEach((peer, i) => {
-            const { sdp } = params.offers && params.offers[i] && params.offers[i].offer || {}
-            const isTrickleSdp = SDP_TRICKLE_REGEX.test(sdp)
-            if (isTrickleSdp) {
-              swarm.offers.set(params.offers[i].offer_id, peer.peerId)
-            }
-
-            peer.socket.send(
-              JSON.stringify({
-                action: "announce",
-                offer: params.offers[i].offer,
-                offer_id: params.offers[i].offer_id,
-                peer_id: common.hexToBinary(params.peer_id),
-                info_hash: common.hexToBinary(params.info_hash),
-              }),
-              peer.socket.onSend
-            )
-            debug("sent offer to %s from %s", peer.peerId, params.peer_id)
-          })
-        }
-
-        server.getSwarm(params.info_hash)
-          .then()
-          .catch(err => server.emit("warning", err))
-
+          peer.socket.send(
+            JSON.stringify({
+              action: 'announce',
+              offer: params.offers[i].offer,
+              offer_id: params.offers[i].offer_id,
+              peer_id: common.hexToBinary(params.peer_id),
+              info_hash: common.hexToBinary(params.info_hash)
+            }),
+            peer.socket.onSend
+          )
+          debug('sent offer to %s from %s', peer.peerId, params.peer_id)
+        })
       }
 
       const done = () => {
@@ -173,43 +150,43 @@ function setupWebSocketServer(server) {
 
       if (params.answer) {
         debug(
-          "got answer %s from %s",
+          'got answer %s from %s',
           JSON.stringify(params.answer),
           params.peer_id
         )
 
         server.getSwarm(params.info_hash)
-              .then(gotSwarm)
-              .catch(err => server.emit("warning", err))
+          .then(gotSwarm)
+          .catch(err => server.emit('warning', err))
 
         const gotSwarm = swarm => {
           if (server.destroyed) return
           if (!swarm) {
             return server.emit(
-              "warning",
-              new Error("no swarm with that `info_hash`")
+              'warning',
+              new Error('no swarm with that `info_hash`')
             )
           }
           // Mark the destination peer as recently used in cache
           const toPeer = swarm.peers.get(params.to_peer_id)
           if (!toPeer) {
             return server.emit(
-              "warning",
-              new Error("no peer with that `to_peer_id`")
+              'warning',
+              new Error('no peer with that `to_peer_id`')
             )
           }
 
           toPeer.socket.send(
             JSON.stringify({
-              action: "announce",
+              action: 'announce',
               answer: params.answer,
               offer_id: params.offer_id,
               peer_id: common.hexToBinary(params.peer_id),
-              info_hash: common.hexToBinary(params.info_hash),
+              info_hash: common.hexToBinary(params.info_hash)
             }),
             toPeer.socket.onSend
           )
-          debug("sent answer to %s from %s", toPeer.peerId, params.peer_id)
+          debug('sent answer to %s from %s', toPeer.peerId, params.peer_id)
 
           done()
         }
@@ -224,7 +201,7 @@ function setupWebSocketServer(server) {
   }
 
   const onWebSocketClose = (socket) => {
-    debug("websocket close %s", socket.peerId)
+    debug('websocket close %s', socket.peerId)
     socket.destroyed = true
 
     if (socket.peerId) {
@@ -233,10 +210,10 @@ function setupWebSocketServer(server) {
         if (swarm) {
           swarm.announce(
             {
-              type: "ws",
-              event: "stopped",
+              type: 'ws',
+              event: 'stopped',
               numwant: 0,
-              peer_id: socket.peerId,
+              peer_id: socket.peerId
             },
             noop
           )
@@ -246,30 +223,30 @@ function setupWebSocketServer(server) {
 
     // ignore all future errors
     socket.onSend = noop
-    socket.on("error", noop)
+    socket.on('error', noop)
 
     socket.peerId = null
     socket.infoHashes = null
 
-    if (typeof socket.onMessageBound === "function") {
-      socket.removeListener("message", socket.onMessageBound)
+    if (typeof socket.onMessageBound === 'function') {
+      socket.removeListener('message', socket.onMessageBound)
     }
     socket.onMessageBound = null
 
-    if (typeof socket.onErrorBound === "function") {
-      socket.removeListener("error", socket.onErrorBound)
+    if (typeof socket.onErrorBound === 'function') {
+      socket.removeListener('error', socket.onErrorBound)
     }
     socket.onErrorBound = null
 
-    if (typeof socket.onCloseBound === "function") {
-      socket.removeListener("close", socket.onCloseBound)
+    if (typeof socket.onCloseBound === 'function') {
+      socket.removeListener('close', socket.onCloseBound)
     }
     socket.onCloseBound = null
   }
 
   const onWebSocketError = (socket, err) => {
-    debug("websocket error %s", err.message || err)
-    server.emit("warning", err)
+    debug('websocket error %s', err.message || err)
+    server.emit('warning', err)
     onWebSocketClose(socket)
   }
 
@@ -281,17 +258,17 @@ function setupWebSocketServer(server) {
   }
 
   ws.address = () => server.http.address()
-  ws.on("connection", onConnection)
-  ws.on("error", (err) => onError(err))
+  ws.on('connection', onConnection)
+  ws.on('error', (err) => onError(err))
 
   server.ws = ws
 }
 
-function createWebSocketServer(server, onListening) {
+function createWebSocketServer (server, onListening) {
   setupHttpService(server, onListening)
   setupWebSocketServer(server)
 }
 
 module.exports = createWebSocketServer
 
-function noop() { }
+function noop () { }
